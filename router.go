f98cbf8388e6bf97b2bd1d8f13a10098a28ca378
jerzy99jerzy <jerzy.siwecki@me.com>
2017-07-10 20:14:44 +0200

    blablabla

---
+++ router.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+
+
+	mux "github.com/julienschmidt/httprouter" 
+)
+
+func NewRouter() *mux.Router {
+
+	router := mux.New()
+
+	for _, route := range routes {
+		
+
+		
+		router.Handle(route.Method, route.Pattern, route.Handle)
+	}
+
+	return router
+}
